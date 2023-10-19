9eaf2b5de418e3877de82d7ded25625ce1621260
acurro9 <asbarxas.aaroncurro@gmail.com>
2023-10-19 16:12:57 +0100

    Se añaden los estilos de los botones y de la tabla

--- admin/index.php
+++ admin/index.php
@@ -22,45 +22,35 @@
         <p class="alerta exito">No se pudo borrar el anuncio</p>
     <?php } ?>
 
-    <table >
-        <tr>
-                <td>Imagen</td>
-                <td>ID</td>
-                <td>Nombre</td>
-                <td>Precio</td>
-                <!--
-                <td>Habitaciones</td>
-                <td>Baños</td>
-                <td>Estacionamiento</td>
-                <td>Descripcion</td>-->
-                <td>Operaciones</td>
-        </tr>
-        <?php
-        while ($fila=mysqli_fetch_assoc($datos)){?>
-            <tr>
-                
-                <td><img src="../imagenes/<?php echo $fila['imagen'] ?>" alt=""></td>
-                <td><?php echo $fila['id'] ?></td>
-                <td><?php echo $fila['titulo'] ?></td>
-                <td><?php echo $fila['precio'] ?></td>
-                <!--
-                <td><?php echo $fila['habitaciones'] ?></td>
-                <td><?php echo $fila['wc'] ?></td>
-                <td><?php echo $fila['estacionamiento'] ?></td>
-                <td><?php echo $fila['descripcion'] ?></td>
-                -->  
-                <td>
-                    <a href="/admin/propiedades/actualizar.php/?id=<?php echo $fila['id']?>" class="boton boton-verde">Actualizar propiedad</a>
-                    <a href="/admin/propiedades/borrar.php/?id=<?php echo $fila['id']?>" class="boton boton-amarillo borrar">Borrar propiedad</a>
-                </td>
-            </tr>
-        <?php } ?>
-                
-    </table>
-        
-
     <a href="/admin/propiedades/crear.php" class="boton boton-verde crear">Nueva propiedad</a>
     <a href="/admin/vendedores/crear.php" class="boton boton-verde crear">Nuevo Vendedor</a>
-    
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Operaciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($fila=mysqli_fetch_assoc($datos)){?>
+                <tr>
+                    
+                    <td><?php echo $fila['id'] ?></td>
+                    <td><?php echo $fila['titulo'] ?></td>
+                    <td><img src="../imagenes/<?php echo $fila['imagen'] ?>" alt=""></td>
+                    <td><?php echo $fila['precio'] ?></td>
+                    <td>
+                        <a href="/admin/propiedades/actualizar.php/?id=<?php echo $fila['id']?>" class="boton-amarillo-block">Actualizar propiedad</a>
+                        <a href="/admin/propiedades/borrar.php/?id=<?php echo $fila['id']?>" class="boton-rojo-block">Borrar propiedad</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>       
+    </table>    
 </main>
 
