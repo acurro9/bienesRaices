1a0de95c2c35882f7544240cf6002e04739ffc01
acurro9 <asbarxas.aaroncurro@gmail.com>
2023-10-19 16:01:23 +0100

    Se añaden los estilos de los botones y de la tabla

--- admin/indexVend.php
+++ admin/indexVend.php
@@ -9,7 +9,7 @@
     $datos=mysqli_query($db,$consult);
 
 ?>
-<link rel="stylesheet" href="./styleTabla.css">
+
 <main class="contenedor seccion">
     <h1>Administrador Vendedores</h1>
     <?php if (intval($resultado)===1){ ?>
@@ -22,33 +22,34 @@
         <p class="alerta exito">No se puede borrar al vendedor porque tiene propiedades bajo su mando</p>
     <?php } ?>
 
-    <table >
-        <tr>
-                <td>ID</td>
-                <td>Nombre</td>
-                <td>Apellidos</td>
-                <td>Nº Telf</td>
-                <td>Operaciones</td>
-        </tr>
-        <?php
-        while ($fila=mysqli_fetch_assoc($datos)){?>
-            <tr>
-                
-                <td><?php echo $fila['id'] ?></td>
-                <td><?php echo $fila['nombre'] ?></td>
-                <td><?php echo $fila['apellidos'] ?></td>
-                <td><?php echo $fila['telefono'] ?></td> 
-                <td>
-                    <a href="/admin/vendedores/actualizar.php/?id=<?php echo $fila['id']?>" class="boton boton-verde">Actualizar vendedor</a>
-                    <a href="/admin/vendedores/borrar.php/?id=<?php echo $fila['id']?>" class="boton boton-amarillo borrar">Borrar vendedor</a>
-                </td>
-            </tr>
-        <?php } ?>
-                
-    </table>
-        
-
     <a href="/admin/vendedores/crear.php" class="boton boton-verde crear">Nuevo Vendedor</a>
     <a href="/admin/index.php" class="boton boton-verde crear">Propiedades</a>
-    
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellidos</th>
+                    <th>Nº Telf</th>
+                    <th>Operaciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($fila=mysqli_fetch_assoc($datos)){?>
+                <tr>
+                    
+                    <td><?php echo $fila['id'] ?></td>
+                    <td><?php echo $fila['nombre'] ?></td>
+                    <td><?php echo $fila['apellidos'] ?></td>
+                    <td><?php echo $fila['telefono'] ?></td> 
+                    <td>
+                        <a href="/admin/vendedores/actualizar.php/?id=<?php echo $fila['id']?>" class="boton-amarillo-block">Actualizar vendedor</a>
+                        <a href="/admin/vendedores/borrar.php/?id=<?php echo $fila['id']?>" class="boton-rojo-block">Borrar vendedor</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>    
 </main>
